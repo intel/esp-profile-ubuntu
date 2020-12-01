c92308c20ff9a985f98b95829f5a8791f1ffccc1
aakash-v-virani <aakash.v.virani@intel.com>
2020-12-01 15:35:47 +0530

    Update package list

--- profile.sh
+++ profile.sh
@@ -12,7 +12,7 @@ PROVISIONER=$1
 
 # --- Add Packages
 ubuntu_bundles="ubuntu-desktop openssh-server"
-ubuntu_packages="net-tools vim software-properties-common apt-transport-https wget linux-generic-hwe-18.04 xserver-xorg-hwe-18.04"
+ubuntu_packages="net-tools vim software-properties-common apt-transport-https wget linux-generic-hwe-18.04 xserver-xorg-core-hwe-18.04"
 
 # --- List out any docker images you want pre-installed separated by spaces. ---
 pull_sysdockerimagelist=""
