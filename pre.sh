89d064b9fe43cc12d2086920cad3c3f84dbc18cf
aakash-v-virani <aakash.v.virani@intel.com>
2021-06-16 14:41:42 +0530

    Add support for docker login

--- pre.sh
+++ pre.sh
@@ -158,6 +158,15 @@ else
 	export kernel_params="$param_kernparam"
 fi
 
+if [[ $kernel_params == *"docker_login_user="* ]]; then
+	tmp="${kernel_params##*docker_login_user=}"
+	export param_docker_login_user="${tmp%% *}"
+fi
+
+if [[ $kernel_params == *"docker_login_pass="* ]]; then
+	tmp="${kernel_params##*docker_login_pass=}"
+	export param_docker_login_pass="${tmp%% *}"
+fi
 
 # --- Get free memory
 export freemem=$(grep MemTotal /proc/meminfo | awk '{print $2}')
@@ -298,6 +307,12 @@ run "Configuring Image Database" \
     "$TMP/provisioning.log"
 
 while (! docker ps > /dev/null ); do sleep 0.5; done
+
+if [ ! -z "${param_docker_login_pass}" ]; then
+    run "Log in to a Docker registry" \
+    "docker login -u ${param_docker_login_user} -p ${param_docker_login_pass}" \
+    "$TMP/provisioning.log"
+fi
 
 # --- Begin Ubuntu Install Process ---
 run "Preparing Ubuntu ${param_ubuntuversion} installer" \
